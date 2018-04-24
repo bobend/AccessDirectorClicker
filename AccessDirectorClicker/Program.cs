2e602195494e43550a8d45f60e7039750b24302f
Bo Bendtsen <bben@dr.dk>
2018-04-24 15:43:40 +0200

    Added logo and error printing.
    Updated README.md

--- AccessDirectorClicker/Program.cs
+++ AccessDirectorClicker/Program.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Security.Principal;
+using System.Threading;
 using System.Windows.Automation;
 
 namespace AccessDirectorClicker
 {
     static class Program
     {
+
+        private static string Logo = @"
+                       _                         _                     
+  /\   _  _  _   _  _ | \ o ._ _   _ _|_  _  ._ /  | o  _ |  |   _  ._ 
+ /--\ (_ (_ (/_ _> _> |_/ | | (/_ (_  |_ (_) |  \_ | | (_ |< |< (/_ |  
+                                                                       
+";
+
         private static readonly string[] ProcessToKill = new[] {"AccessDirectorFramework", "AccessDirectorTray"};
         public static IEnumerable<AutomationElement> EnumNotificationIcons()
         {
@@ -23,43 +32,61 @@ namespace AccessDirectorClicker
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Finding Access Director tray icon...");
-            var found = false;
-            foreach (var icon in EnumNotificationIcons())
-            {
-                var name = (string) icon.GetCurrentPropertyValue(AutomationElement.NameProperty);
-                Console.WriteLine(name);
-                if (!name.StartsWith("Access Director")) continue;
-                Console.WriteLine("Click!");
-                icon.InvokeButton();
-                found = true;
-                break;
-            }
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine(Logo);
+            Console.WriteLine();
 
-            if (!found)
+            try
             {
-                Console.WriteLine("Access Director not found.");
-            }
-            else
-            {
-                if (IsAdministrator())
+                Console.WriteLine("Finding Access Director tray icon...");
+                var found = false;
+                foreach (var icon in EnumNotificationIcons())
                 {
-                    foreach (var s in ProcessToKill)
-                    {
-                        foreach (var process in Process.GetProcessesByName(s))
-                        {
-                            Console.Write($"Found {s} with id {process.Id}, killing it...");
-                            process.Kill();
-                            Console.WriteLine("Done");
-                        }
-                    }
+                    var name = (string) icon.GetCurrentPropertyValue(AutomationElement.NameProperty);
+                    Console.WriteLine(name);
+                    if (!name.StartsWith("Access Director")) continue;
+                    Console.WriteLine("Click!");
+                    icon.InvokeButton();
+                    found = true;
+                    break;
+                }
+
+                if (!found)
+                {
+                    Console.WriteLine("Access Director not found.");
                 }
                 else
                 {
-                    Console.WriteLine("Not administrator, can not kill Access Director");
+                    if (IsAdministrator())
+                    {
+                        foreach (var s in ProcessToKill)
+                        {
+                            foreach (var process in Process.GetProcessesByName(s))
+                            {
+                                Console.Write($"Found {s} with id {process.Id}, killing it...");
+                                process.Kill();
+                                Console.WriteLine("Done");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not administrator, can not kill Access Director");
+                    }
                 }
+
+                // Console.ReadKey();
             }
-            // Console.ReadKey();
+            catch (Exception e)
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error: ");
+                Console.WriteLine(e.Message);
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 
 # AccessDirectorClicker
 Small tool to automatically click and kill Access Director
+
+## Usage
+Add to task scheduler as a start-up task to run at log-on with delayed start (1-2 minutes) and with heighest privileges.
